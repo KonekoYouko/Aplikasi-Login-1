6fe290f9b81d71804538e707b09aa6bd0e0a9997
Koneko Youko <konekoyouko@gmail.com>
2022-11-05 15:40:37 +0800

    Update login-script.js
    Update JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,11 +1,3 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
-
 /** @module Login-Script */
 /**
 * Membuat variabel loginFormElement untuk tampilan form.
@@ -46,13 +38,11 @@ const expectedPassword = 'superpassword';
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  
   /**
    * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
    * @constant {string}
    */
   const email = inputEmailElement.value;
-  
   
   /**
    * Membuat variabel password untuk menyimpan nilai password yang didapatkan saat button ditekan.
